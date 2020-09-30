d9604400eb9058cbfa0fdfb512e1798f5fe8c8f7
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-09-30 23:33:16 +0300

    Add exercise

---
+++ Streams, Files and Directories/Lab/01.OddLines/01.OddLines/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
