70102fb1260f3b9f3765993a395287efdef50e17
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-10-03 19:09:31 +0300

    Add exercise

---
+++ Streams, Files and Directories/Exercise/05.DirectoryTraversal/05.DirectoryTraversal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> report = new Dictionary<string, Dictionary<string, double>>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../../");
+
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (!report.ContainsKey(file.Extension))
+                {
+                    report.Add(file.Extension, new Dictionary<string, double>());
+                }
+
+                report[file.Extension].Add(file.Name, file.Length / 1024.00);
+            }
+
+            using (StreamWriter writer = new StreamWriter(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\report.txt"))
+            {
+                foreach (var file in report.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine(file.Key);
+
+                    foreach (var info in file.Value.OrderBy(x => x.Value))
+                    {
+                        writer.WriteLine($"--{info.Key} - {info.Value}kb");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories/Exercise/06.ZipAndExtract/06.ZipAndExtract/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"E:\FileDirectory", @"E:\CreateZipFile\myZipFile");
+            ZipFile.ExtractToDirectory(@"E:\CreateZipFile\myZipFile", @"E:\ExtractZipFile");
+        }
+    }
+}
