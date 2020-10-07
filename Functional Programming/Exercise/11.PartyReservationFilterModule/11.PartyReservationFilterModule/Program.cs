643aa3e3a0a92e5b4a50339a11ed9156fcf4d5ed
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-10-07 15:37:21 +0300

    Add exercise

---
+++ Functional Programming/Exercise/11.PartyReservationFilterModule/11.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            string input = string.Empty;
+
+            List<string> filteredNames = new List<string>();
+
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                string[] splittedInput = input.Split(";");
+
+                string command = splittedInput[0];
+                string filter = splittedInput[1];
+                string parameter = splittedInput[2];
+
+                var predicate = GetPredicate(filter, parameter);
+
+                if (command == "Add filter")
+                {
+                    var temp = names.FindAll(predicate);
+
+                    filteredNames.AddRange(temp);
+                }
+                else if (command == "Remove filter")
+                {
+                    filteredNames.RemoveAll(predicate);
+                }
+            }
+
+            for (int i = 0; i < filteredNames.Count; i++)
+            {
+                names.Remove(filteredNames[i]);
+            }
+
+            Console.WriteLine(string.Join(' ', names));
+        }
+
+        static Predicate<string> GetPredicate(string filter, string parameter)
+        {
+            switch (filter)
+            {
+                case "Starts with": return x => x.StartsWith(parameter);
+                case "Ends with": return x => x.EndsWith(parameter);
+                case "Length": return x => x.Length == int.Parse(parameter);
+                default: return x => x.Contains(parameter);
+            }
+        }
+    }
+}
---
+++ Functional Programming/Exercise/12.TriFunction/12.TriFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split();
+
+            Func<string, int, bool> firstValidName = (name, sum) =>
+              {
+                  int sumOfChars = 0;
+
+                  for (int i = 0; i < name.Length; i++)
+                  {
+                      sumOfChars += name[i];
+                  }
+
+                  return sum <= sumOfChars;
+              };
+
+            Func<string[], int, Func<string, int, bool>, string> result = (names, sum, func) =>
+             {
+                 return names.FirstOrDefault(x => func(x, sum));
+             };
+
+            Console.WriteLine(result(input, n, firstValidName));
+        }
+    }
+}
