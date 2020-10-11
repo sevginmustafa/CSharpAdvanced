3e0d199f33942b2dd8760230ef4de164b9905c4d
Sevgin Mustafa <sevgin_6_1996@abv.bg>
2020-10-11 22:16:05 +0300

    Add exercise

---
+++ Defining Classes/Execise/10.SoftUniParking/10.SoftUniParking/Parking.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public Parking(int capacity)
+        {
+            cars = new List<Car>();
+            this.capacity = capacity;
+        }
+
+        List<Car> cars;
+        int capacity;
+
+        public int Count => cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (capacity == cars.Count)
+            {
+                return "Parking is full!";
+            }
+
+            cars.Add(car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (cars.Any(x => x.RegistrationNumber == registrationNumber))
+            {
+                Car remove = GetCar(registrationNumber);
+                cars.Remove(remove);
+                return $"Successfully removed {registrationNumber}";
+            }
+
+            return "Car with that registration number, doesn't exist!";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return cars.FirstOrDefault(x => x.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var regNum in registrationNumbers)
+            {
+                RemoveCar(regNum);
+            }
+        }
+    }
+}
