7125298022966b17c97773c63421b2c73899b831
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2020-11-04 14:59:26 +0200

    Add exercise

---
+++ Algorithms Introduction/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            MergeSort<int>.Sort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+
+        }
+    }
+}
---
+++ Algorithms Introduction/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == String.Empty)
+            {
+                return;
+            }
+
+            int[] arr = input
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Quick.Sort(arr);
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
